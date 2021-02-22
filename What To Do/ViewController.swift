8007a1e302684f449aff2d77a2299cadd57be439
rifqialfaizi <rifqialfaizii@gmail.com>
2021-02-22 18:56:15 +0700

    Initial Commit

---
+++ What To Do/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  What To Do
+//
+//  Created by Rifqi Alfaizi on 22/02/21.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
